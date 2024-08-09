163de465b39dd0ea3edc6efb04fab1e18c03e8c6
VuTrungNghia <vunghia467@gmail.com>
2024-08-09 11:21:18 +0700

    createProject

---
+++ src/main/java/com/example/RestApiPayment/RestApiPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestApiPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestApiPayment/RestApiPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestApiPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
